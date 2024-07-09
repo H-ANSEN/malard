bd84e83171e6ba4e779d385addf555868c2f8bf7
teague hansen <thanse23@asu.edu>
2024-07-09 19:26:43 -0400

    move io functions to header

--- kernel/io.h
+++ kernel/io.h
@@ -1,7 +1,24 @@
 #ifndef __IO__
 #define __IO__
 
-void outb(unsigned short port, unsigned char data);
-unsigned char inb(unsigned short port);
+inline __attribute__((always_inline)) 
+void outb(unsigned short port, unsigned char data) {
+    asm volatile (
+        "outb %0, %1"
+        :
+        : "a"(data), "Nd"(port)
+    );
+}
+
+inline __attribute__((always_inline)) 
+unsigned char inb(unsigned short port) {
+    unsigned char result;
+    asm volatile (
+        "inb %1, %0"
+        : "=a"(result)
+        : "Nd"(port)
+    );
+    return result;
+}
 
 #endif /* __IO__ */
